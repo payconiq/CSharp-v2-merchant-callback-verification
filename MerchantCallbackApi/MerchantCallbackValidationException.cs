8ce41d4cedfe282856169c3ccbf5e72edce2bf93
Halleh Khoshnevis <hallehkhoshnevis@ip-10-15-1-240.eu-central-1.compute.internal>
2018-01-04 13:26:43 +0100

    verifying the signature based on the provided info and public key of the certificate

---
+++ MerchantCallbackApi/MerchantCallbackValidationException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace MerchantCallbackApi
+{
+    /**
+    * Class to specify the exceptions which can occure in the signature verification
+    **/
+    public class MerchantCallbackValidationException : Exception
+    {
+        public MerchantCallbackValidationException(String message) : base(message) 
+        {}
+    }
+}
---
+++ MerchantCallbackApi/TestMerchantCallbackApi.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace MerchantCallbackApi
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            String signedSignature = "IrJO4pIA8mhRKAHM7731FmZNDfBIcdce/4H9N22mldZZEWgfLcolc6xEIaEtHvCcZ1HVdL1KKeXp10FdwAq/3NVHY1RkoBMstUtdQQ9UgKnFovIkGhYm3u7N5G9YM7iivX3AOMiBSCBgTAVsan731e57LTc3Q1klWlSJwyqdwncYYvXv41Aov0UpnzE0UWgmJjmffbezHZOxiWP/dXZV5sCkYSvK5oe0WbLGsJzR8yFuByNCfrv6NDN6V45YvnicXJ1+CPnuR6cEngCxmQEYE0K668IaD3B2zblLiTb89b23ft/E8LaUsM2iNEgI7f1LuL7FzNW09KrPYgDFPFurHA==";
+            String merchantId = "5981e8a4c9716c3dca30679d";
+            String timestamp = "2017-08-30T13:57:48.726Z";
+            String body = "{\"_id\":\"59a6c431f9285003bd46fe56\",\"status\":\"SUCCEEDED\"}";
+            String hashAlgorithm = MerchantCallbackApiConstants.CRYPTOGRAPHIC_HASH_ALGORITHM_SHA256;
+            String certLocation = @"../../TestCertificate/payconiq.crt";
+
+            MerchantCallbackApi merchantCallbackApi = new MerchantCallbackApi(merchantId, timestamp, body, hashAlgorithm);
+            Console.WriteLine(merchantCallbackApi.VerifySignature(signedSignature, certLocation));
+
+           
+        }
+    }
+}
+
